69ab3aa59aa6a55a8930eb82b7578eeb8dd224d1
Evie-Mututa <evemututa29@gmail.com>
2023-10-08 14:20:36 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+
+var customerName = `bob`;
+function upperCaseCustomerName() {
+    if (typeof customerName === 'string') {
+        customerName = customerName.toUpperCase();
+      } else {
+        console.log('customerName is not a valid string.');
+      }
+    }
+// code to set the best customer
+function setBestCustomer() {
+    bestCustomer = `not bob`
+}
+
+// Define the overwriteBestCustomer function
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+// Declare the leastFavoriteCustomer variable as global
+var leastFavoriteCustomer;
+
+//change least favourite customer function
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'someone else';
+}
+
+module.exports = {
+    customerName,
+    upperCaseCustomerName,
+    setBestCustomer,
+    overwriteBestCustomer,
+    changeLeastFavoriteCustomer
+};
